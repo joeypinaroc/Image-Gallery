eaed0aa6d50a6057fbc3e17d0bfdba4e8c842569
Joey Pinaroc <jmpinaroc123@gmail.com>
2024-02-14 11:58:28 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var imageBox = document.getElementById("imageBox");
+
+var leftButton = document.getElementById("left");
+var rightButton = document.getElementById("right");
+
+var imagesNumber = 3;
+var imageCounter = 1;
+imageBox.setAttribute("src", `/img${imageCounter}.png`);
+leftButton.addEventListener("click", ()=>{
+    if(imageCounter == 1)
+    {
+        imageCounter = imagesNumber;
+    }
+    else
+    {
+        imageCounter -= 1;
+    }
+    imageBox.setAttribute("src", `/img${imageCounter}.png`);
+})
+
+rightButton.addEventListener("click", ()=>{
+    if(imageCounter == imagesNumber)
+    {
+        imageCounter = 1;
+    }
+    else
+    {
+        imageCounter += 1;
+    }
+    imageBox.setAttribute("src", `/img${imageCounter}.png`);
+})
